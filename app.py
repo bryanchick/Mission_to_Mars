815ebc6b8bd4dd036a2bd9358eeb04447f0e0e10
Bryan <dbryanchick@gmail.com>
2019-03-03 16:47:47 -0700

    another update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from flask import Flask, render_template, redirect
 from flask_pymongo import PyMongo
-import mars_scrape
+# import mars_scrape
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/test_mars"
@@ -13,15 +13,20 @@ def homepage():
     return render_template("index.html", result = result)
 
 
-@app.route("/scrape")
-def scrape():
-    pass
-    # that function returns a dicitionary
-    result = mars_scrape.scrape_master()
-    mongo.db.test_collection.update({}, result, upsert=True)
-    return redirect("/", 302)
+# @app.route("/scrape")
+# def scrape():
+#     pass
+#     # that function returns a dicitionary
+#     result = mars_scrape.scrape_master()
+#     mongo.db.test_collection.update({}, result, upsert=True)
+#     return redirect("/", 302)
 
-
+# @app.route('/images/<filename>')
+# def image(filename):
+#     fs = gridfs.GridFS(db)
+#     gridout = fs.get_last_version(filename=filename)
+#     response.content_type = 'image/jpeg'
+#     return gridout
 
 
 
--- scrape_mars.py
+++ scrape_mars.py
@@ -5,19 +5,19 @@ from bs4 import BeautifulSoup
 
 # Dealing with the headline
 
-def get_images():
-    # your functin should bulild this list of dictionaries
-    # return it as a dict, having a key of images
+# def get_images():
+#     # your functin should bulild this list of dictionaries
+#     # return it as a dict, having a key of images
     
-    return {"images": [
-        {"title": "Valles Marineris Hemisphere", "img_url": "..."},
-        {"title": "Cerberus Hemisphere", "img_url": "..."},
-        {"title": "Schiaparelli Hemisphere", "img_url": "..."},
-        {"title": "Syrtis Major Hemisphere", "img_url": "..."},
-]
+#     return {"images": [
+#         {"title": "Valles Marineris Hemisphere", "img_url": "..."},
+#         {"title": "Cerberus Hemisphere", "img_url": "..."},
+#         {"title": "Schiaparelli Hemisphere", "img_url": "..."},
+# #         {"title": "Syrtis Major Hemisphere", "img_url": "..."},
+# ]
 
     
-    }
+#     }
 
 def get_mars_headline():
     url = "https://mars.nasa.gov/api/v1/news_items/?page=0&per_page=40&order=publish_date+desc%2Ccreated_at+desc&search=&category=19%2C165%2C184%2C204&blank_scope=Latest"
@@ -35,12 +35,12 @@ def get_mars_facts_table():
     return {"html_table_facts": formatted}
 
 
-def scrape_master():
-    print('scraping stuff')
-    facts_table_dict = get_mars_facts_table()
-    headlines_dict = get_mars_headline()
-    image_dict = get_images()
-    merged_dict = {**facts_table_dict, **headlines_dict, **image_dict}
-    print('done merging')
-    # merged dict will be the new data in mongodb
-    return merged_dict
+# def scrape_master():
+#     print('scraping stuff')
+#     facts_table_dict = get_mars_facts_table()
+#     headlines_dict = get_mars_headline()
+#     image_dict = get_images()
+#     merged_dict = {**facts_table_dict, **headlines_dict, **image_dict}
+#     print('done merging')
+#     # merged dict will be the new data in mongodb
+#     return merged_dict
